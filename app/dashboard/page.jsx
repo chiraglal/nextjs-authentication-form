819e648def7e58f295b572e2a385f23b463c18d6
CHIRAG LAL <chiraglal15@gmail.com>
2024-07-22 20:41:16 +0530

    first commit

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const dashboardPage = () => {
+    return (
+        <>
+            <div className="text-2xl font-bold mb-5">
+                <p className='mb-5'>
+                    Welcome to the Dashboard
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default dashboardPage
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="font-bold mb-5 text-2xl">Welcome</h1>
+      <p className="mb-5">Registration Form Made With Next.js + Tailwind CSS + Clerk Authentication</p>
+    </>
+  );
+}
